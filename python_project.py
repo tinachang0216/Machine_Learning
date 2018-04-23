e32639dd65e7b638b53c17b3095589fd82399a2a
tinachang0216 <changty1994@gwu.edu>
2018-04-23 15:07:54 -0700

    Add files via upload

---
+++ python_project.py
@@ -0,0 +1,4 @@
+df = psql.read_sql(('select "Timestamp","Value" from "MyTable" '
+                     'where "Timestamp" BETWEEN %(dstart)s AND %(dfinish)s'),
+                   db,params={"dstart":datetime(2014,6,24,16,0),"dfinish":datetime(2014,6,24,17,0)},
+                   index_col=['Timestamp'])
