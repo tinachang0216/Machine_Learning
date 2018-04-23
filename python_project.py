59fcd8d37a639e0ce475656d7fa1289ab8d9359d
tinachang0216 <changty1994@gwu.edu>
2018-04-23 15:08:15 -0700

    Delete python_project.py

--- python_project.py
+++
@@ -1,4 +0,0 @@
-df = psql.read_sql(('select "Timestamp","Value" from "MyTable" '
-                     'where "Timestamp" BETWEEN %(dstart)s AND %(dfinish)s'),
-                   db,params={"dstart":datetime(2014,6,24,16,0),"dfinish":datetime(2014,6,24,17,0)},
-                   index_col=['Timestamp'])
